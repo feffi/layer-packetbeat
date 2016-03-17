2fd9c744fcf1fd53f98ac0c0b4edfc78800a1d43
Charles Butler <chuck@dasroot.net>
2016-03-17 13:16:54 -0400

    Initial commit

---
+++ reactive/packetbeat.py
@@ -0,0 +1,53 @@
+from charms.reactive import when
+from charms.reactive import when_file_changed
+from charms.reactive import when_not
+from charms.reactive import set_state
+from charms.reactive import remove_state
+
+from charmhelpers.core.hookenv import status_set
+from charmhelpers.core.host import service_restart
+from charmhelpers.fetch import apt_install
+
+from elasticbeats import render_without_context
+from elasticbeats import enable_beat_on_boot
+from elasticbeats import push_beat_index
+
+
+@when('beat.repo.available')
+@when_not('packetbeat.installed')
+def install_packetbeat():
+    status_set('maintenance', 'Installing packetbeat')
+    apt_install(['packetbeat'], fatal=True)
+    set_state('filebeat.installed')
+
+
+@when('beat.render')
+def render_packetbeat_template():
+    render_without_context('packetbeat.yml', '/etc/filebeat/packetbeat.yml')
+    remove_state('beat.render')
+    status_set('active', 'Packetbeat ready')
+
+
+@when('config.changed.install_sources')
+@when('config.changed.install_keys')
+def reinstall_packetbeat():
+    remove_state('packetbeat.installed')
+
+
+@when_file_changed('/etc/filebeat/packetbeat.yml')
+def restart_packetbeat():
+    ''' Anytime we touch the config file, cycle the service'''
+    service_restart('packetbeat')
+
+
+@when('packetbeat.installed')
+@when_not('packetbeat.autostarted')
+def enlist_packetbeat():
+    enable_beat_on_boot('packetbeat')
+    set_state('packetbeat.autostarted')
+
+
+@when('elasticsearch.available')
+@when_not('packetbeat.index.pushed')
+def push_packetbeat_index():
+    push_beat_index('packetbeat')
