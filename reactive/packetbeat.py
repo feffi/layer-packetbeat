d615b2e26f67813cddfe737fd096a98ddfe9261b
Charles Butler <chuck@dasroot.net>
2016-06-01 13:30:46 -0400

    Bump to get packetbeat functional
    Update the layer, metdata for series, config w/ the apt repositories, the template indentation for packetbeat config, and finally the states in the reactive class

--- reactive/packetbeat.py
+++ reactive/packetbeat.py
@@ -1,43 +1,31 @@
+import charms.apt
 from charms.reactive import when
-from charms.reactive import when_file_changed
 from charms.reactive import when_not
+from charms.reactive import when_any
 from charms.reactive import set_state
 from charms.reactive import remove_state
 
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.host import service_restart
-from charmhelpers.fetch import apt_install
 
 from elasticbeats import render_without_context
 from elasticbeats import enable_beat_on_boot
 from elasticbeats import push_beat_index
 
 
-@when('beats.repo.available')
-@when_not('packetbeat.installed')
-def install_packetbeat():
+@when_not('apt.installed.packetbeat')
+def install_filebeat():
     status_set('maintenance', 'Installing packetbeat')
-    apt_install(['packetbeat'], fatal=True)
-    set_state('packetbeat.installed')
+    charms.apt.queue_install(['packetbeat'])
 
 
 @when('beat.render')
-def render_packetbeat_template():
+@when_any('elasticsearch.available', 'logstash.available')
+def render_filebeat_template():
     render_without_context('packetbeat.yml', '/etc/packetbeat/packetbeat.yml')
     remove_state('beat.render')
-    status_set('active', 'Packetbeat ready')
-
-
-@when('config.changed.install_sources')
-@when('config.changed.install_keys')
-def reinstall_packetbeat():
-    remove_state('packetbeat.installed')
-
-
-@when_file_changed('/etc/filebeat/packetbeat.yml')
-def restart_packetbeat():
-    ''' Anytime we touch the config file, cycle the service'''
     service_restart('packetbeat')
+    status_set('active', 'Packetbeat ready')
 
 
 @when('packetbeat.installed')
